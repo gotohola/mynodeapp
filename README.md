7d0f7657e305c3d9afc11c511ac52052033ca19e
Goto Hola <gotohola67@gmail.com>
2025-04-03 14:21:22 +0200

    Añadido trabajo con redis

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-hola
+##Reiniciar certificados##
+sudo microk8s stop
+sudo microk8s refresh-certs --cert ca.crt
+sudo microk8s start
+
+
+
+###Logeo##
+argocd login localhost:8080 \
+  --username admin \
+  --password "contraseña" \
+  --insecure
+'admin:login' logged in successfully
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const Redis = require('ioredis');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+const redisHost = process.env.REDIS_HOST || 'localhost';
+const redis = new Redis({ host: redisHost });
 
 app.get('/', (req, res) => {
   res.send('Hello from myNodeApp!');
+});
+
+app.get('/count', async (req, res) => {
+  try {
+    // incrementa una key "visits" en Redis
+    const newCount = await redis.incr('visits');
+    res.send(`Visitas: ${newCount}`);
+  } catch (error) {
+    res.status(500).send('Error conectando con Redis');
+  }
 });
 
 app.get('/health', (req, res) => {
